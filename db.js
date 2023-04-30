7e5eeb1c554f6143e477985fd94cec72108019a0
Viet Tung <Viettung150803@gmail.com>
2023-04-30 14:29:00 +0700

    fix database

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1/social_app", {
+    await mongoose.connect(process.env.DATABASE_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
